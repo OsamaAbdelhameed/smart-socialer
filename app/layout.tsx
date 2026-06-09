462d2e49552af423961bff15b8420b29362d0c2c
Osama Abdelhameed <osamaabdelhameed41@gmail.com>
2026-06-09 13:44:54 +0800

    chore: rename application from Lemon.ai to Smart Socialer

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Lemon.ai | Social Media Scheduling",
+  title: "Smart Socialer | Social Media Scheduling",
   description:
-    "Create AI-powered social media scheduling for every platform in seconds. Lemon.ai is a platform that allows you to create social media scheduling for every platform in seconds.",
+    "Create AI-powered social media scheduling for every platform in seconds. Smart Socialer is a platform that allows you to create social media scheduling for every platform in seconds.",
 };
 
 export default function RootLayout({
